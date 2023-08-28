ebc743eeae539b034fd0c13fa971584b92a6e59e
laskrzys <133368910+laskrzys@users.noreply.github.com>
2023-08-28 21:41:02 +0000

    buttons working and input form added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from database import returnOsobaTableDict
 
 app = Flask(__name__)
@@ -24,7 +24,16 @@ def bosman():
   return render_template('bosman.html')
 
 
-@app.route("/listaosob")
+@app.route("/bosman/addosoba")
+def addosoba():
+  return render_template('addosoba.html')
+
+@app.route("/bosman/bosman/addosoba/add", methods=['post'])
+def addosobaAdd():
+  data = request.form
+  return jsonify(data)
+
+@app.route("/bosman/listaosob")
 def listaosob():
   return render_template('listaosob.html', osobaTable=returnOsobaTableDict())
 
