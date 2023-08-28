bfda31c84ac132ecef4b56b9a7f4e498982d1790
laskrzys <133368910+laskrzys@users.noreply.github.com>
2023-08-28 09:17:40 +0000

    added buttons, css and członek page

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def helloWorld():
   return render_template('home.html')
-
+@app.route("/członek")
+def członek():
+  return render_template('członek.html')
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
