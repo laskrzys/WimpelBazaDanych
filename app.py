eb6ce211fa41567962e7a485e5e156d38e949f9e
laskrzys <133368910+laskrzys@users.noreply.github.com>
2023-09-06 10:37:08 +0000

    some more changes, nearly final?

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from database import returnOsobaTableDict, addosobatodb, droposobaindp
+from database import returnOsobaTableDict, addosobatodb, droposobaindp, raport1indb, raport2indb, raport3indb, returnStanicaTableDict
 
 app = Flask(__name__)
 
@@ -28,25 +28,70 @@ def bosman():
 def addosoba():
   return render_template('addosoba.html')
 
+
 @app.route("/bosman/bosman/addosoba/add", methods=['post'])
 def doneaddosoba():
   data = request.form
   addosobatodb(data)
   return render_template('doneaddosoba.html', osobadata=data)
 
+
 @app.route("/bosman/listaosob")
 def listaosob():
   return render_template('listaosob.html', osobaTable=returnOsobaTableDict())
 
+
 @app.route("/bosman/deleteosoba")
 def deleteosoba():
   return render_template('deleteosoba.html', osobaTable=returnOsobaTableDict())
-  
+
+
 @app.route("/bosman/deleteosoba/delete", methods=['post'])
 def donedeletosoba():
   idOsoba = request.form
   droposobaindp(idOsoba)
+  print(idOsoba)
   return render_template('donedeleteosoba.html', osobadata=idOsoba)
+
+
+@app.route("/bosman/raport1")
+def raport1():
+  return render_template('raport1.html', osobaTable=returnOsobaTableDict())
+
+
+@app.route("/bosman/bosman/raport1/raport", methods=['post'])
+def raport1raport():
+  data = request.form
+  result = raport1indb(data)
+  result2 = result[0]
+  return render_template('raport1raport.html', result=result2)
+
+
+@app.route("/bosman/raport2")
+def raport2():
+  return render_template('raport2.html', osobaTable=returnOsobaTableDict())
+
+
+@app.route("/bosman/bosman/raport2/raport", methods=['post'])
+def raport2raport():
+  data = request.form
+  result = raport2indb(data)
+  result2 = result[0]
+  return render_template('raport2raport.html', result=result2)
+
+@app.route("/bosman/raport3")
+def raport3():
+  return render_template('raport3.html', stanicaTable=returnStanicaTableDict())
+
+
+@app.route("/bosman/bosman/raport3/raport", methods=['post'])
+def raport3raport():
+  data = request.form
+  result = raport3indb(data)
+  result2 = result[0]
+  print(result)
+  return render_template('raport3raport.html', result=result2)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine, text
 import os
+
 connectionString = os.environ['connectionString']
 engine = create_engine(connectionString,
-                      connect_args={
-                        "ssl": {
-                          "ssl_ca": "/etc/ssl/cert.pem"
-                        }
-                      })
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def returnOsobaTableDict():
   with engine.connect() as conn:
@@ -18,15 +18,32 @@ def returnOsobaTableDict():
       OsobaTableDict.append(row._mapping)
     return OsobaTableDict
 
+
+def returnStanicaTableDict():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Stanica"))
+    StanicaTableDict = []
+    for row in result.all():
+      StanicaTableDict.append(row._mapping)
+    return StanicaTableDict
+
+
 def addosobatodb(osobadata):
   with engine.connect() as conn:
-    query = text("INSERT INTO Osoba (Imie, Nazwisko, Email, Telefon) VALUES (:Imie, :Nazwisko, :Email, :Telefon)")
-    _Imie=osobadata['Imie']
-    _Nazwisko=osobadata['Nazwisko']
-    _Email=osobadata['Email']
-    _Telefon=osobadata['Telefon']
+    query = text(
+      "INSERT INTO Osoba (Imie, Nazwisko, Email, Telefon) VALUES (:Imie, :Nazwisko, :Email, :Telefon)"
+    )
+    _Imie = osobadata['Imie']
+    _Nazwisko = osobadata['Nazwisko']
+    _Email = osobadata['Email']
+    _Telefon = osobadata['Telefon']
 
-    conn.execute(statement=query, parameters=dict(Imie=_Imie, Nazwisko=_Nazwisko, Email=_Email,         Telefon=_Telefon))
+    conn.execute(statement=query,
+                 parameters=dict(Imie=_Imie,
+                                 Nazwisko=_Nazwisko,
+                                 Email=_Email,
+                                 Telefon=_Telefon))
+
 
 def droposobaindp(idosoba):
   with engine.connect() as conn:
@@ -34,3 +51,47 @@ def droposobaindp(idosoba):
     _idosoba = idosoba['Id']
     print(idosoba)
     conn.execute(statement=query, parameters=dict(idosoba=_idosoba))
+
+
+def raport1indb(data):
+  with engine.connect() as conn:
+    query = text(
+      "SELECT COUNT (*) as LiczbaNapraw FROM Uszkodzenie WHERE DataNaprawy >= :dataod AND idCzlonek = :idosoba"
+    )
+    _idosoba = data['idosoby']
+    _dataod = data['dataod']
+    result = conn.execute(statement=query,
+                          parameters=dict(idosoba=_idosoba, dataod=_dataod))
+    results_as_dict = result.mappings().all()
+    return results_as_dict
+
+
+def raport2indb(data):
+  with engine.connect() as conn:
+    query = text(
+      "SELECT COUNT (*) as LiczbaSzkod FROM Uszkodzenie WHERE DataZepsucia >= :dataod AND idCzlonek = :idosoba"
+    )
+    _idosoba = data['idosoby']
+    _dataod = data['dataod']
+    result = conn.execute(statement=query,
+                          parameters=dict(idosoba=_idosoba, dataod=_dataod))
+    results_as_dict = result.mappings().all()
+    return results_as_dict
+
+
+def raport3indb(data):
+  with engine.connect() as conn:
+    query1 = text("""
+CREATE TEMP VIEW IF NOT EXISTS OstatnieZepsucie AS SELECT Przedmiot.nazwa, Przedmiot.idstanica, MAX(Uszkodzenie.datazepsucia) as ostatniTest FROM Przedmiot RIGHT JOIN Uszkodzenie USING(idprzedmiot) WHERE Przedmiot.stan = 'smiec' GROUP BY Przedmiot.idprzedmiot
+ """)
+    query2 = ("""
+    SELECT COUNT(nazwa) as ileUsunieto FROM OstatnieZepsucie WHERE idstanica = :idStanicy AND ostatniTest >= :dataOd;
+    """)
+    _idstanicy = data['idstanicy']
+    _dataod = data['dataod']
+    conn.execute(query1)
+    result = conn.execute(statement=query2,
+                          parameters=dict(idStanicy=_idstanicy,
+                                          dataOd=_dataod))
+    results_as_dict = result.mappings().all()
+    return results_as_dict
